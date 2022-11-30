86bdd9320abd3305a5daa46a12ce219b9ad238f1
MQuirke <micheal.quirke2@students.ittralee.ie>
2022-11-30 19:18:19 +0000

    Initial commit

---
+++ JavaScriptProject/jsCode.js
@@ -0,0 +1,87 @@
+window.onload = loadStuff;
+
+let houseTypes = ["Bungalow","Country House","Detached","Semi Detached","Terraced","Flat"]
+let coverTypes = ["Owner occupied","Holiday","Rental"]
+let totalCost = 0, valueA = 0, valueB = 0, valueC = 0;
+let total = document.getElementById("runningTotal")
+const date = new Date().getFullYear();
+
+function loadStuff(){
+    document.getElementById("runningTotal").value = totalCost;
+    let option = document.createElement("OPTION");
+    let houseList = document.getElementById("houseType");
+    let coverList = document.getElementById("coverType");
+    let contentsCoverList = document.getElementById("contentsCover");
+    let buildingCoverList = document.getElementById("buildingCover");
+
+    for (let i=0;i<houseTypes.length;i++){
+        option = document.createElement("OPTION");
+        option.text = houseTypes[i];
+        option.value = houseTypes[i];
+        houseList.options.add(option);
+    }
+    for (let i=0;i<coverTypes.length;i++){
+        option = document.createElement("OPTION");
+        option.text = coverTypes[i];
+        option.value = coverTypes[i];
+        coverList.options.add(option);
+    }
+    for (let i=1;i<11;i++){
+        option = document.createElement("OPTION");
+        option.text = i*5000;
+        option.value = i*5000;
+        contentsCoverList.options.add(option);
+    }
+    for (let i=1;i<11;i++){
+        option = document.createElement("OPTION");
+        option.text = i*100000;
+        option.value = i*100000;
+        buildingCoverList.options.add(option);
+    }
+
+    document.getElementById("calculate").addEventListener("click",validateAndUpdate)
+    return true;
+}
+function validateAndUpdate(){
+    let ownerShip = false;
+
+    if (document.getElementById("Tenet").checked) {
+        valueA = 50;
+        ownerShip = true;
+    }
+    else if (document.getElementById("Owner").checked) {
+        valueA = 100;
+        ownerShip = true;
+    }
+    else{
+        ownerShip = false;
+    }
+
+
+
+
+    if (ownerShip === true){
+        if (isNumerical(((document.getElementById("numOfRooms").value).toString()))){
+            document.getElementById("runningTotal").value = (valueA + valueB + valueC).toString();
+        }
+        else {
+            window.alert("Error, number of rooms entered is invalid");
+            document.getElementById("runningTotal").value = 0;
+        }
+    }
+    else{
+        window.alert("Error enter Tenant or Owner");
+        document.getElementById("runningTotal").value = 0;
+    }
+}
+
+function isNumerical(String){
+    let valid = true;
+    for (let i = 0;i<String.length;i++) {
+        if (!isNaN(String.charAt(i))) {
+            valid = false;
+            break;
+        }
+    }
+    return valid;
+}
